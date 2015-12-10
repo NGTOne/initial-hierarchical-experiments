6528953e2942e68a8dbcf61b863ad1be6f7c7d19
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-10 01:54:47 -0500

    Added experiment code for ES

--- compile-scripts/setup-experiments.sh
+++ compile-scripts/setup-experiments.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm -rf experiments
+rm -rf experiments-to-run
 
-mkdir experiments
+mkdir experiments-to-run
 
 categories=$(ls src/experiments)
 
 for cat in $categories
 do
-	mkdir experiments/$cat
+	mkdir experiments-to-run/$cat
 	srcdir=src/experiments/$cat
 	experiments=$(ls $srcdir)
 	experiments=$(echo $experiments | sed 's/\.cpp//g')
@@ -16,6 +16,6 @@ do
 	for exper in $experiments
 	do
 		g++ -c -g -std=gnu++0x -fPIC -Iinclude $srcdir/$exper.cpp -o obj/$cat/$exper.o
-		g++ -std=gnu++0x -Iinclude -lHierGA -o experiments/$cat/$exper obj/*.o obj/$cat/$exper.o
+		g++ -std=gnu++0x -Iinclude -lHierGA -o experiments-to-run/$cat/$exper obj/*.o obj/$cat/$exper.o
 	done
 done
