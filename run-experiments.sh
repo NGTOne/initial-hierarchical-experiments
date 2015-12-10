98633d3dde542195b1509f23a98a153887a3677a
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-10 12:34:25 -0500

    Added a script to run experiments

---
+++ run-experiments.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run each experiment this many times
+count=$1
+categories=$(ls experiments-to-run)
+
+rm -rf experiment-results
+mkdir experiment-results
+
+for cat in $categories
+do
+	mkdir experiment-results/$cat
+	experiments=$(ls experiments-to-run/$cat)
+
+	for exper in $experiments
+	do
+		mkdir experiment-results/$cat/$exper
+
+		for i in $(seq 1 $count)
+		do
+			experiments-to-run/$cat/$exper > experiment-results/$cat/$exper/run-$i.txt
+		done
+	done
+done
