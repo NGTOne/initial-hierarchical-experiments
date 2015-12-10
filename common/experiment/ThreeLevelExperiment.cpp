2403b70b1b0e3899d33ae1514f0c4c62e919e8db
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-09 21:21:18 -0500

    Add 3-level experiment class

---
+++ common/experiment/ThreeLevelExperiment.cpp
@@ -0,0 +1,32 @@
+#include "./TwoLevelExperiment.hpp"
+
+TwoLevelExperiment(FitnessFunction * objective, ToStringFunction * objectiveTS, FitnessFunction * promise, ToStringFunction * promiseTS, GenerationModel * model) {
+	SelectionStrategy * strategy = new TournamentSelection(0.5);
+	CrossoverOperation * crossover = new NPointCrossover(2);
+	MutationOperation * mutation = new UniformMutation(0.1);
+
+	// Set up the library nodes
+	int bits[] = {0, 1};
+	GenePool * baseGenes = new NonHierarchicalGenePool<int>(bits, 2);
+	GenePool ** libraries = (GenePool**)malloc(sizeof(GenePool*)*2);
+	for (int i = 0; i < 2; i++) libraries[i] = baseGenes;
+
+
+	GenePool ** midNodes = (GenePool**)malloc(sizeof(GenePool*)*4);
+
+	for (int i = 0; i < 4; i++) {
+		Individual * templateIndividual = new Individual(libraries, 2, crossover, mutation, promise, promiseTS);
+		GenePool ** bottomNodes = (GenePool**)malloc(sizeof(GenePool*)*4);
+		for (int k = 0; k < 4; k++) {
+			bottomNodes[k] = new HierarchicalGenePool(4, templateIndividual, 100, 1, model, NULL, new NonPropagator());
+		}
+
+		templateIndividual = new Individual(bottomNodes, 4, crossover, mutation, promise, promiseTS);
+		midNodes[i] = new HierarchicalGenePool(8, templateIndividual, 100, 1, model, NULL, new NonPropagator());
+	}
+
+
+	templateIndividual = new Individual(midNodes, 4, crossover, mutation, objective, objectiveTS);
+
+	topLevelPool = new HierarchicalGenePool(8, templateIndividual, 100, 1, model, NULL, new NonPropagator());
+}
