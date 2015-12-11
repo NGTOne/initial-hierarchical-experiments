7dce082cacdf32ba6c2787e31471ceb892c84714
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-11 00:43:58 -0500

    Added a script to just do analysis of existing results

---
+++ analyze-experiments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+categories=$(ls experiment-results)
+
+for cat in $categories
+do
+	experiments=$(ls experiment-results/$cat)
+
+	for exper in $experiments
+	do
+		perl analyze-convergence.pl experiment-results/$cat/$exper 30
+	done
+done
