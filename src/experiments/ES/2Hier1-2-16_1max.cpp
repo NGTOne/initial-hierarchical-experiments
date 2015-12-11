d2da0c5b70420a61e46cdb30e51c4a8d3d87495e
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2015-12-10 21:32:43 -0500

    Added 2-level experiments

---
+++ src/experiments/ES/2Hier1-2-16_1max.cpp
@@ -0,0 +1,9 @@
+#include "problem/1maxProblem.hpp"
+
+int main(void) {
+	SelectionStrategy * strategy = new TournamentSelection(0.5);
+	GenerationModel * model = new ESGeneration(strategy);
+
+	Problem * problem = new OneMaxProblem(model, 2, 0, 2, 16);
+	problem->run();
+}
